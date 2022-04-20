3dba20ce1572361ee9c30bf7b4c5fc99f284f62a
erfansalima <erfansalima@gmail.com>
2022-04-20 16:35:39 +0430

    City nearly finished

--- src/main/java/model/City.java
+++ src/main/java/model/City.java
@@ -1,7 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class City {
     private String name;
     private Civilization owner;
     private int neededProduction;
+    private Unit unitInProgress;
+    private int numberOfCitizen;
+    private HashMap<Character, Integer> coordinatesOfCenter = new HashMap<>();
+    private int foodStorage;
+    private int foodPerTurn;
+    private int productionPerTurn;
+    private int goldPerTurn;
+    private int sciencePerTurn;
+    //private ArrayList<Hex> cityHexes = new ArrayList<>();
+    //hex not added
+    private RangedMilitary cityUnit;
+    //rangedmilitary not added
+
+    public City(String name, int x, int y){
+        this.name = name;
+        // complete coordinate
+    }
+
 }
