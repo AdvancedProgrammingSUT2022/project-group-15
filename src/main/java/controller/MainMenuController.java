b52018f412a7185f94511cb7e45607b508e77bb6
amirmahdi <amirmm71382@gmail.com>
2022-04-20 14:50:57 +0430

    adding classes

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import model.User;
+
+import java.util.ArrayList;
+
 public class MainMenuController extends Controller{
+    public void startGame (ArrayList<User>){
+
+    }
 }
