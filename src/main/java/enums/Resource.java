f96eb983f8e2ec07cc46c043e0cb2e3e6242bfe8
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2022-05-02 22:59:08 +0430

    add type to resource enum

--- src/main/java/enums/Resource.java
+++ src/main/java/enums/Resource.java
@@ -4,36 +4,46 @@ package enums;
  * @author Parsa
  */
 public enum Resource {
-    BANANA("banana", 1, 0, 0),
-    COW("cow", 1, 0, 0),
-    GAZELLE("gazelle", 1, 0, 0),
-    SHEEP("sheep", 2, 0, 0),
-    WHEAT("wheat", 1, 0, 0),
-    COAL("coal", 0, 1, 0),
-    HORSE("horse", 0, 1, 0),
-    IRON("iron", 0, 1, 0),
-    COTTON("cotton", 0, 0, 2),
-    COLOR("color", 0, 0, 2),
-    FUR("fur", 0, 0, 2),
-    GEM("gem", 0,0, 3),
-    GOLD("gold", 0, 0, 2),
-    FUMIGATION("fumigation", 0, 0, 2),
-    TUSK("tusk", 0, 0, 2),
-    MARBLE("marble", 0, 0, 2),
-    SILK("silk", 0, 0, 2),
-    SILVER("silver", 0, 0, 2),
-    SUGAR("sugar", 0, 0, 2);
+    BANANA("banana", 1, 0, 0, "scoring"),
+    COW("cow", 1, 0, 0, "scoring"),
+    GAZELLE("gazelle", 1, 0, 0, "scoring"),
+    SHEEP("sheep", 2, 0, 0, "scoring"),
+    WHEAT("wheat", 1, 0, 0, "scoring"),
+    COAL("coal", 0, 1, 0, "strategic"),
+    HORSE("horse", 0, 1, 0, "strategic"),
+    IRON("iron", 0, 1, 0, "strategic"),
+    COTTON("cotton", 0, 0, 2, "luxury"),
+    COLOR("color", 0, 0, 2, "luxury"),
+    FUR("fur", 0, 0, 2, "luxury"),
+    GEM("gem", 0,0, 3, "luxury"),
+    GOLD("gold", 0, 0, 2, "luxury"),
+    FUMIGATION("fumigation", 0, 0, 2, "luxury"),
+    TUSK("tusk", 0, 0, 2, "luxury"),
+    MARBLE("marble", 0, 0, 2, "luxury"),
+    SILK("silk", 0, 0, 2, "luxury"),
+    SILVER("silver", 0, 0, 2, "luxury"),
+    SUGAR("sugar", 0, 0, 2, "luxury");
 
     public final String name;
     public final int food;
     public final int production;
     public final int gold;
 
-    Resource(String name, int food, int production, int gold) {
+    /*
+     1. scoring (can not be exchanged with other civilizations)
+     2. luxury (add 4 points to the civilization's happiness (only for the first time))
+     3. strategic (require a technology to be shown on the screen / if appropriate improvement applied on a hex that includes this,
+        some of this resource will be gained and can be used to build units and buildings / after removing a unit or a building ,
+        the used resources will be return to the civilization)
+    */
+    public final String type;
+
+    Resource(String name, int food, int production, int gold, String type) {
         this.name = name;
         this.food = food;
         this.gold = gold;
         this.production = production;
+        this.type = type;
     }
 
     @Override
