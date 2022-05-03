0891b652d66a5493ebd334902a3a3763f6aa0217
erfansalima <erfansalima@gmail.com>
2022-05-03 20:51:16 +0430

    compiling error fixed

--- src/main/java/enums/Improvement.java
+++ src/main/java/enums/Improvement.java
@@ -1,44 +1,42 @@
 package enums;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public enum Improvement {
-    CAMP("camp", 0, 0, 0, Technology.TRAPPING,
-            new ArrayList<>(List.of(Terrain.TUNDRA, Terrain.PLAIN, Terrain.HILL)), new ArrayList<>(List.of(Feature.JUNGLE))),
-    FARM("farm", 1, 0, 0, Technology.AGRICULTURE,
-            new ArrayList<>(List.of(Terrain.DESERT, Terrain.PLAIN, Terrain.GRASSLAND)), new ArrayList<>()),
-    LUMBER_MILL("lumber mill", 0, 0, 1, Technology.MASONRY,
-            new ArrayList<>(), new ArrayList<>(List.of(Feature.JUNGLE))),
-    MINE("mine", 1, 0, 1, Technology.MINING,
-            new ArrayList<>(List.of(Terrain.DESERT, Terrain.PLAIN, Terrain.GRASSLAND, Terrain.TUNDRA, Terrain.SNOWLAND, Terrain.HILL)), new ArrayList<>(List.of(Feature.JUNGLE, Feature.DENSE_FOREST, Feature.SWAMP))),
-    PASTURE("pasture", 0, 0, 0, Technology.ANIMAL_HUSBANDRY,
-            new ArrayList<>(List.of(Terrain.TUNDRA, Terrain.PLAIN, Terrain.HILL, Terrain.GRASSLAND, Terrain.DESERT)), new ArrayList<>()),
-    PLANTATION("plantation", 0, 0, 0, Technology.CALENDAR,
-            new ArrayList<>(List.of(Terrain.DESERT, Terrain.PLAIN, Terrain.GRASSLAND)), new ArrayList<>(List.of(Feature.JUNGLE, Feature.DENSE_FOREST, Feature.SWAMP, Feature.FLAT))),
-    QUARRY("quarry", 0, 0, 0, Technology.MASONRY,
-            new ArrayList<>(List.of(Terrain.TUNDRA, Terrain.PLAIN, Terrain.HILL, Terrain.DESERT, Terrain.GRASSLAND)), new ArrayList<>()),
-    TRADING_POST("trading post", 0, 1, 0, Technology.TRAPPING,
-            new ArrayList<>(List.of(Terrain.TUNDRA, Terrain.PLAIN, Terrain.DESERT, Terrain.GRASSLAND)), new ArrayList<>()),
-    FACTORY("factory", 0, 0, 2, Technology.ENGINEERING,
-            new ArrayList<>(List.of(Terrain.TUNDRA, Terrain.PLAIN, Terrain.DESERT, Terrain.GRASSLAND, Terrain.SNOWLAND)), new ArrayList<>()),
-    ROAD("road", 0, 0, 0, null, new ArrayList<>(), new ArrayList<>()),
-    RAILROAD("railroad", 0, 0, 0, null, new ArrayList<>(), new ArrayList<>());
+    CAMP("camp", 0, 0, 0,
+            new ArrayList<>(Arrays.asList(Terrain.TUNDRA, Terrain.PLAIN, Terrain.HILL)), new ArrayList<>(Arrays.asList(Feature.JUNGLE))),
+    FARM("farm", 1, 0, 0,
+            new ArrayList<>(Arrays.asList(Terrain.DESERT, Terrain.PLAIN, Terrain.GRASSLAND)), new ArrayList<>()),
+    LUMBER_MILL("lumber mill", 0, 0, 1,
+            new ArrayList<>(), new ArrayList<>(Arrays.asList(Feature.JUNGLE))),
+    MINE("mine", 1, 0, 1,
+            new ArrayList<>(Arrays.asList(Terrain.DESERT, Terrain.PLAIN, Terrain.GRASSLAND, Terrain.TUNDRA, Terrain.SNOWLAND, Terrain.HILL)), new ArrayList<>(Arrays.asList(Feature.JUNGLE, Feature.DENSE_FOREST, Feature.SWAMP))),
+    PASTURE("pasture", 0, 0, 0,
+            new ArrayList<>(Arrays.asList(Terrain.TUNDRA, Terrain.PLAIN, Terrain.HILL, Terrain.GRASSLAND, Terrain.DESERT)), new ArrayList<>()),
+    PLANTATION("plantation", 0, 0, 0,
+            new ArrayList<>(Arrays.asList(Terrain.DESERT, Terrain.PLAIN, Terrain.GRASSLAND)), new ArrayList<>(Arrays.asList(Feature.JUNGLE, Feature.DENSE_FOREST, Feature.SWAMP, Feature.FLAT))),
+    QUARRY("quarry", 0, 0, 0,
+            new ArrayList<>(Arrays.asList(Terrain.TUNDRA, Terrain.PLAIN, Terrain.HILL, Terrain.DESERT, Terrain.GRASSLAND)), new ArrayList<>()),
+    TRADING_POST("trading post", 0, 1, 0,
+            new ArrayList<>(Arrays.asList(Terrain.TUNDRA, Terrain.PLAIN, Terrain.DESERT, Terrain.GRASSLAND)), new ArrayList<>()),
+    FACTORY("factory", 0, 0, 2,
+            new ArrayList<>(Arrays.asList(Terrain.TUNDRA, Terrain.PLAIN, Terrain.DESERT, Terrain.GRASSLAND, Terrain.SNOWLAND)), new ArrayList<>()),
+    ROAD("road", 0, 0, 0, new ArrayList<>(), new ArrayList<>()),
+    RAILROAD("railroad", 0, 0, 0, new ArrayList<>(), new ArrayList<>());
 
     public final String name;
     public final int food;
     public final int gold;
     public final int production;
-    public final Technology technology;
     public final ArrayList<Terrain> terrains;
-    private final ArrayList<Feature> features;
+    public final ArrayList<Feature> features;
 
-    Improvement(String name, int food, int gold, int production, Technology technology, ArrayList<Terrain> terrains, ArrayList<Feature> features) {
+    Improvement(String name, int food, int gold, int production, ArrayList<Terrain> terrains, ArrayList<Feature> features) {
         this.name = name;
         this.food = food;
         this.gold = gold;
         this.production = production;
-        this.technology = technology;
         this.terrains = terrains;
         this.features = features;
     }
