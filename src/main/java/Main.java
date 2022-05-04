3e46659d4c384219b5502aa6f0ce5793a452c7c9
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2022-05-04 23:32:53 +0430

    last commit of checkpoint 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,21 +8,40 @@ import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
-        GameMenuController gameMenuController = new GameMenuController();
-        ArrayList<User> users = new ArrayList<>();
-        users.add(new User("","","0"));
-        users.add(new User("","","1"));
-        users.add(new User("","","2"));
-        users.add(new User("","","3"));
-        Game.startNewGame(users);
-        System.out.println(Game.getGame().getSelectedCivilization().getUnits().get(0).getCoordinatesInMap().get('x')+ " "+
-                Game.getGame().getSelectedCivilization().getUnits().get(0).getCoordinatesInMap().get('y'));
-        System.out.println(gameMenuController.showMapOnPosition(3,3));
-        System.out.println(gameMenuController.moveMap("right",1));
-        gameMenuController.showMap();
-        GameMenuController controller2 = new GameMenuController();
-        System.out.println(controller2.showTechnologyInfo());
         Controller controller = new Controller();
         controller.run();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        GameMenuController gameMenuController = new GameMenuController();
+//        ArrayList<User> users = new ArrayList<>();
+//        users.add(new User("","","0"));
+//        users.add(new User("","","1"));
+//        users.add(new User("","","2"));
+//        users.add(new User("","","3"));
+//        Game.startNewGame(users);
+//        System.out.println(Game.getGame().getSelectedCivilization().getUnits().get(0).getCoordinatesInMap().get('x')+ " "+
+//                Game.getGame().getSelectedCivilization().getUnits().get(0).getCoordinatesInMap().get('y'));
+//        System.out.println(gameMenuController.showMapOnPosition(3,3));
+//        gameMenuController.showMap();
+//        GameMenuController controller2 = new GameMenuController();
+//        System.out.println(controller2.showTechnologyInfo());
