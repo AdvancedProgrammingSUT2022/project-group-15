952ecad2e0994cfa8d33ab213d631e01859e2801
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2022-05-13 21:17:51 +0430

    unit tests for ProfileMenuController completed

--- src/test/java/controller/ProfileMenuControllerTest.java
+++ src/test/java/controller/ProfileMenuControllerTest.java
@@ -1,17 +1,13 @@
 package controller;
 
-import model.Game;
 import model.User;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -50,5 +46,36 @@ public class ProfileMenuControllerTest {
         matcher = Pattern.compile(regex).matcher(command);
         System.out.println("matches : " + matcher.matches());
         Assert.assertEquals("user with nickname parsabashari already exists", controller.changeNickname(matcher));
+    }
+
+    @Test
+    public void changePasswordSuccessfulTest(){
+        regex = "^(?<currentPassword>\\S+) (?<newPassword>\\S+)$";
+        command = "oldPassword1234 newPassword1234";
+        matcher = Pattern.compile(regex).matcher(command);
+        System.out.println("matches : " + matcher.matches());
+        theMock.when(() -> User.getLoggedInUser()).thenReturn(user);
+
+        Assert.assertEquals("password changed successfully!", controller.changePassword(matcher));
+    }
+
+    @Test
+    public void changePasswordInvalidPasswordTest(){
+        regex = "^(?<currentPassword>\\S+) (?<newPassword>\\S+)$";
+        command = "oldPassword newPassword1234";
+        matcher = Pattern.compile(regex).matcher(command);
+        System.out.println("matches : " + matcher.matches());
+        theMock.when(() -> User.getLoggedInUser()).thenReturn(user);
+        Assert.assertEquals("current password is invalid", controller.changePassword(matcher));
+    }
+
+    @Test
+    public void changePasswordCurrentPasswordTest(){
+        regex = "^(?<currentPassword>\\S+) (?<newPassword>\\S+)$";
+        command = "oldPassword1234 oldPassword1234";
+        matcher = Pattern.compile(regex).matcher(command);
+        System.out.println("matches : " + matcher.matches());
+        theMock.when(() -> User.getLoggedInUser()).thenReturn(user);
+        Assert.assertEquals("please enter a new password", controller.changePassword(matcher));
     }
 }
