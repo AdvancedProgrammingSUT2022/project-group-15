03ac7f5d571303490ff6d3644421218ef7e2d0cb
amirmahdi <amirmm71382@gmail.com>
2022-06-03 17:40:01 +0430

    adding fx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 import controller.Controller;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main extends Application{
+    public static void main (String[] args) {
+        launch();
         Controller controller = new Controller();
         controller.run();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
     }
 }
