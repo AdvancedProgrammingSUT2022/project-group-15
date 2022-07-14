00d055c6a3be6cc1e79e1317c0f3d1025dede2a9
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2022-07-14 20:15:39 +0430

    add hex assets

--- src/main/java/view/GameMenu.java
+++ src/main/java/view/GameMenu.java
@@ -6,9 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import model.Hex;
 
 import java.io.IOException;
 import java.net.URL;
@@ -39,6 +41,11 @@ public class GameMenu extends Menu implements Initializable {
             }
         }
         return scene;
+    }
+
+    public Image[] graphicalHex(Hex hex) {
+        // TODO : Parsa
+        return null;
     }
 
 }
