acb0d586306feac49c263d06a9737f5bafce07a2
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2022-07-15 16:59:32 +0430

    add image property to Terrain.java and Feature.java

--- src/main/java/enums/Feature.java
+++ src/main/java/enums/Feature.java
@@ -1,19 +1,21 @@
 package enums;
 
+import javafx.scene.image.Image;
+
 /**
  * @author Parsa
  */
 public enum Feature {
     // movement price -1 means that units cannot pass by this hex
     // movement price -2 means that units should spend all of their remained moves to pass by this hex
-    FLAT("flat", 2, 0, 0, -33, 1),
-    JUNGLE("jungle", 1, 1, 0, 25, 2),
-    ICE("ice", 0, 0, 0, 0, -1),
-    DENSE_FOREST("dense forest", 1, -1, 0, 25, 2),
-    SWAMP("swamp", -1, 0, 0, -33, 2),
-    OASIS("oasis", 3, 0, 1, -33, 1),
-    RIVER("river", 0, 0, 0, 0, -2),
-    NULL("null",0,0,0,0,0);
+    FLAT("flat", 2, 0, 0, -33, 1, "/tiles/003Clear1.png"),
+    JUNGLE("jungle", 1, 1, 0, 25, 2, "/tiles/Jungle4.png"),
+    ICE("ice", 0, 0, 0, 0, -1, "/tiles/Ice1.png"),
+    DENSE_FOREST("dense forest", 1, -1, 0, 25, 2, "/tiles/Woods4_1.png"),
+    SWAMP("swamp", -1, 0, 0, -33, 2, "/tiles/Lake3.png"),
+    OASIS("oasis", 3, 0, 1, -33, 1, "/tiles/Arid_Brush05.png"),
+    RIVER("river", 0, 0, 0, 0, -2, "/tiles/River1-4.png"),
+    NULL("null",0,0,0,0,0, "/tiles/003Clear1.png");
 
     public final String name;
     public final int food;
@@ -21,14 +23,16 @@ public enum Feature {
     public final int gold;
     public final int combatEffect;
     public final int movementPrice;
+    public final Image image;
 
-    Feature(String name, int food, int production, int gold, int combatEffect, int movementPrice) {
+    Feature(String name, int food, int production, int gold, int combatEffect, int movementPrice, String imagePath) {
         this.name = name;
         this.food = food;
         this.production = production;
         this.gold = gold;
         this.combatEffect = combatEffect;
         this.movementPrice = movementPrice;
+        this.image = new Image(getClass().getResource(imagePath).toExternalForm());
     }
 
     @Override
--- src/main/java/enums/Terrain.java
+++ src/main/java/enums/Terrain.java
@@ -1,18 +1,20 @@
 package enums;
 
+import javafx.scene.image.Image;
+
 /**
  * @author Parsa
  */
 public enum Terrain {
     // movement price -1 means that units cannot pass by this hex
-    DESERT("desert", 0, 0, 0, -33, 1),
-    GRASSLAND("grassland", 2, 0, 0, -33, 1),
-    HILL("hill", 0, 2, 0, 25, 2),
-    MOUNTAIN("mountain", 0, 0, 0, 25, -1),
-    OCEAN("ocean", 0, 0, 0, 25, -1),
-    PLAIN("plain", 1, 1, 0, -33, 1),
-    SNOWLAND("snowland", 0, 0, 0, -33, 1),
-    TUNDRA("tundra", 1, 0, 0, -33, 1);
+    DESERT("desert", 0, 0, 0, -33, 1, "/tiles/Arid_Clear05.png"),
+    GRASSLAND("grassland", 2, 0, 0, -33, 1, "/tiles/Grass1.png"),
+    HILL("hill", 0, 2, 0, 25, 2, "/tiles/Hills2.png"),
+    MOUNTAIN("mountain", 0, 0, 0, 25, -1, "/tiles/Mountains1.png"),
+    OCEAN("ocean", 0, 0, 0, 25, -1, "/tiles/Ocean1.png"),
+    PLAIN("plain", 1, 1, 0, -33, 1, "/tiles/Brambles5.png"),
+    SNOWLAND("snowland", 0, 0, 0, -33, 1, "/tiles/SnowLandClear05.png"),
+    TUNDRA("tundra", 1, 0, 0, -33, 1, "/tiles/Snow5.png");
 
     public final String name;
     public final int food;
@@ -20,14 +22,16 @@ public enum Terrain {
     public final int gold;
     public final int combatEffect;
     public final int movementPrice;
+    public final Image image;
 
-    Terrain(String name, int food, int production, int gold, int combatEffect, int movementPrice) {
+    Terrain(String name, int food, int production, int gold, int combatEffect, int movementPrice, String imagePath) {
         this.name = name;
         this.food = food;
         this.production = production;
         this.gold = gold;
         this.combatEffect = combatEffect;
         this.movementPrice = movementPrice;
+        this.image = new Image(getClass().getResource(imagePath).toExternalForm());
     }
 
     @Override
