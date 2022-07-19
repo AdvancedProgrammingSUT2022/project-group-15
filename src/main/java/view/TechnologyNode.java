de9ce4c8d214fc21771ea430cc4f70dfa05aca32
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2022-07-19 14:54:33 +0430

    Technology Tree 90 percent implemented

--- src/main/java/view/TechnologyNode.java
+++ src/main/java/view/TechnologyNode.java
@@ -2,11 +2,31 @@ package view;
 
 import enums.Technology;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import model.Game;
 
 public class TechnologyNode extends Button {
     Technology technology;
 
     public TechnologyNode(Technology technology) {
         this.technology = technology;
+        this.setLayoutX(technology.x);
+        this.setLayoutY(technology.y);
+        this.setBackground(Background.EMPTY);
+        this.setPrefWidth(260);
+        this.setPrefHeight(60);
+        updateNode();
+    }
+
+    public void updateNode() {
+        this.setDisable(true);
+        if (technology == Game.getGame().getSelectedCivilization().getTechnologyInProgress()) {
+            this.setStyle("-fx-border-color: blue");
+        } else if (Game.getGame().getSelectedCivilization().getTechnologies().contains(technology)) {
+            this.setStyle("-fx-border-color: gold");
+        } else if (Game.getGame().getSelectedCivilization().getAvailableTechnologies().contains(technology)) {
+            this.setStyle("-fx-border-color: green");
+            setDisable(false);
+        }
     }
 }
