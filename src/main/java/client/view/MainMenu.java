dfab9fe3165b2ab79753e6f8d42df61eba069f54
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2022-07-25 20:18:33 +0430

    ye chizi 2

--- src/main/java/client/view/MainMenu.java
+++ src/main/java/client/view/MainMenu.java
@@ -30,9 +30,9 @@ public class MainMenu extends Menu implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        avatarImage.imageProperty().bind(((User)Controller.send("getLoggedInPlayer")).avatarProperty());
-        username.textProperty().bind(((User)Controller.send("getLoggedInPlayer")).usernameProperty());
-        score.textProperty().bind(((User)Controller.send("getLoggedInPlayer")).scoreProperty().asString());
+        avatarImage.setImage(((User)Controller.send("getLoggedInPlayer")).getAvatar().image);
+        username.setText(User.getLoggedInUser().getUsername());
+        score.setText(String.valueOf(User.getLoggedInUser().getScore()));
         Tooltip.install(exitButton, new Tooltip("Exit"));
     }
 
