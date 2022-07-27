ec08b08c704ed9bbd7a25b437c0c25317dfe1f7a
ERFAN <erfansalima@gmail.com>
2022-07-27 07:35:23 +0430

    friendship is almost done

--- src/main/java/client/view/FriendshipMenu.java
+++ src/main/java/client/view/FriendshipMenu.java
@@ -1,29 +1,39 @@
 package client.view;
 
 import client.controller.Controller;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class FriendshipMenu extends Menu {
+
     @FXML
-    public AnchorPane pane;
-    public Group friendRequests;
-    public Group searchPlayer;
     public VBox friendRequestBox;
     public VBox searchNameFounded;
     public Pane friendsNamesBox;
-    public Group friendsName;
     public javafx.scene.control.TextField text;
+    public javafx.scene.control.Label popup;
+    public Text names;
+    javafx.scene.control.Button button = new javafx.scene.control.Button("accept");
+    javafx.scene.control.Button button1 = new javafx.scene.control.Button("reject");
+    public ArrayList<String> reqs = new ArrayList<>();
+    javafx.scene.control.Label label = new javafx.scene.control.Label();
 
 
     public Scene getScene() {
@@ -37,16 +47,81 @@ public class FriendshipMenu extends Menu {
     }
 
     public void initialize() throws IOException {
+        reqs = (ArrayList<String>) Controller.send("allReqs", Controller.getMyUser().getUsername());
+        button.setStyle("-fx-stroke: green;");
+        button1.setStyle("-fx-stroke: red;");
+        if (reqs.size() > 0) {
+            label.setText(reqs.get(0));
+            label.setFont(Font.font(15));
+            friendRequestBox.setSpacing(10);
+            friendRequestBox.getChildren().add(label);
+            friendRequestBox.getChildren().add(button);
+            friendRequestBox.getChildren().add(button1);
+            friendRequestBox.setAlignment(Pos.CENTER);
+        }
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Controller.send("delete", Controller.getMyUser().getUsername());
+                friendRequestBox.getChildren().remove(button);
+                friendRequestBox.getChildren().remove(button1);
+                friendRequestBox.getChildren().remove(label);
+                updateAll();
+                friendRequestBox.setVisible(false);
+            }
+        });
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Controller.send("deleteAgain", Controller.getMyUser().getUsername());
+                friendRequestBox.getChildren().remove(button);
+                friendRequestBox.getChildren().remove(button1);
+                friendRequestBox.getChildren().remove(label);
+                friendRequestBox.setVisible(false);
+                updateAll();
+            }
+        });
 
+    }
+
+    public void updateAll() {
+        if (reqs.size() > 0) {
+            Text text = new Text();
+            javafx.scene.control.Button button = new javafx.scene.control.Button("accept");
+            javafx.scene.control.Button button1 = new javafx.scene.control.Button("reject");
+            javafx.scene.control.Label label = new javafx.scene.control.Label();
+            label.setText(reqs.get(0));
+            label.setFont(Font.font(15));
+            friendRequestBox.setSpacing(10);
+            friendRequestBox.getChildren().add(label);
+            friendRequestBox.getChildren().add(button);
+            friendRequestBox.getChildren().add(button1);
+            friendRequestBox.setVisible(true);
+        }
+        ArrayList<String> allFriends = (ArrayList<String>) Controller.send("friend", Controller.getMyUser().getUsername());
+        String name = "";
+        for (int i = 0; i < allFriends.size(); i++) {
+            name += allFriends.get(i) + "\t";
+            if (i % 4 == 0 && i > 0) name += "\n";
+        }
+        names.setText(name);
+        names.setVisible(true);
     }
 
 
     public String send(MouseEvent mouseEvent) {
         String name = text.getText();
-        String out = (String) Controller.send("sendReq" , name , Controller.getMyUser().getUsername());
+        String out = (String) Controller.send("sendReq", name, Controller.getMyUser().getUsername());
+        popup.setText(out);
+        popup.setVisible(true);
         System.out.println(out);
         return out;
     }
 
 
+    public void openMainMenu(MouseEvent mouseEvent) {
+        setup(friendRequestBox);
+        Controller.send("change menu Main");
+        window.setScene(Controller.getMainMenu().getScene());
+    }
 }
--- src/main/java/server/controller/FriendshipController.java
+++ src/main/java/server/controller/FriendshipController.java
@@ -1,16 +1,40 @@
 package server.controller;
 
+import client.controller.Controller;
 import server.model.User;
+
+import java.util.ArrayList;
 
 public class FriendshipController {
 
     public String sendReq(String username, String name) {
-        System.out.println("find");
         if (User.getUserByUsername(username) == null) {
             return "not found";
         } else {
-            User.getUserByUsername(username).getFriendReqs().add(name);
-            return "friend req sent";
+            if (username.equals(name)) {
+                return "another";
+            } else if (User.getUserByUsername(username).getFriendReqs().contains(name)) {
+                return "already sent";
+            } else {
+                User.getUserByUsername(username).getFriendReqs().add(name);
+                return "friend req sent";
+            }
         }
+    }
+    public ArrayList<String> allReqs(String name){
+        return User.getUserByUsername(name).getFriendReqs();
+    }
+    public void delete(String name){
+        User.getUserByUsername(User.getUserByUsername(name).getFriendReqs().get(0)).getFriends().add(name);
+        User.getUserByUsername(name).getFriends().add(User.getUserByUsername(name).getFriendReqs().get(0));
+        User.getUserByUsername(name).getFriendReqs().remove(0);
+    }
+    public void deleteAgain(String name){
+        System.out.println("sallllllam");
+        User.getUserByUsername(name).getFriendReqs().remove(0);
+    }
+    public ArrayList<String> friend(String name){
+        System.out.println("salam");
+        return User.getUserByUsername(name).getFriends();
     }
 }
